1d300e6c4971e5eb1257a9ebd587337fd0eff32f
daFwaak <tmgrboy1004@gmail.com>
2025-04-03 14:32:56 +0545

    changes

--- src/app/apiUrls.js
+++ src/app/apiUrls.js
@@ -1,5 +1,5 @@
-export const base = 'http://192.168.1.69:5555';
-export const baseUrl = 'http://192.168.1.69:5555/api';
+export const base = 'https://bookstore-back-l63x.onrender.com/';
+export const baseUrl = 'https://bookstore-back-l63x.onrender.com/api';
 
 //export const base = 'http://192.168.1.104:5555';
 //export const baseUrl = 'http://192.168.1.104:5555/api';
