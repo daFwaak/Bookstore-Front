e8667eef0ffa7e3b319ec6812dde619665fb9d51
daFwaak <tmgrboy1004@gmail.com>
2025-04-03 17:12:05 +0545

    responsive

--- src/features/book/BookList.jsx
+++ src/features/book/BookList.jsx
@@ -10,7 +10,7 @@ const BookList = () => {
 
   return (
     <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         {data && data.map((book) => <BookCard key={book._id} book={book} />)}
       </div>
     </div>
